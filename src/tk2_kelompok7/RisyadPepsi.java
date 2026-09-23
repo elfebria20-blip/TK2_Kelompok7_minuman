77f8a5db8b9d9d98a3ae427191b39870e889d0e8
Risyad <risyadkhadafii@gamil.com>
2026-09-23 16:04:00 +0700

    add class risyad

--- src/tk2_kelompok7/RisyadPepsi.java
+++ src/tk2_kelompok7/RisyadPepsi.java
@@ -4,7 +4,64 @@ package tk2_kelompok7;
 public class RisyadPepsi {
   
     
-    //
-    
     //Atribut
+    private String nama;
+    private String ukuran;
+    private String harga;
+    private String rasa;
+    private String kemasan;
+    
+    //construktor
+    public RisyadPepsi(String nama, String ukuran, String harga, String rasa, String kemasan) {
+        this.nama = nama;
+        this.ukuran = ukuran;
+        this.harga = harga;
+        this.rasa = rasa;
+        this.kemasan = kemasan;
+    }
+    
+    //setter
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public void setRasa(String rasa) {
+        this.rasa = rasa;
+    }
+
+    public void setKemasan(String kemasan) {
+        this.kemasan = kemasan;
+    }
+    
+    //getter
+    public String getNama() {
+        return nama;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public String getRasa() {
+        return rasa;
+    }
+
+    public String getKemasan() {
+        return kemasan;
+    }
+    
+    
+
 }
