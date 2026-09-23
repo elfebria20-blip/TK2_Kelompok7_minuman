0e8a5f84733a86a15fc4980a07a02e4188f85d2c
elok <elfebria20@gmail.com>
2026-09-23 22:12:25 +0700

    add main elok

---
+++ src/tk2_kelompok7/Minuman.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tk2_kelompok7;
+
+/**
+ *
+ * @author macproi52019
+ */
+public class Minuman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Coca Cola
+        ElokCocacola cocacola = new ElokCocacola("Cocacola", "Sedang", "14.000", "Cola", "Botol");
+        
+        
+        System.out.println("Nama     :" + cocacola.getNama());
+        System.out.println("Ukuran   :" + cocacola.getUkuran());
+        System.out.println("Harga    :Rp " + cocacola.getHarga());
+        System.out.println("Rasa     :" + cocacola.getRasa());
+        System.out.println("Kemasan  :" + cocacola.getKemasan());
+        
+        System.out.println();
+    }
+    
+}
